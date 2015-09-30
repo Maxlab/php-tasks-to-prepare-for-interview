ca26af200e5784bf9d445407d1fd4de3219b36dd
Nikolay Stepanov <forsag1982@gmail.com>
2015-09-30 16:48:36 +0300

    st3

--- test.php
+++ test.php
@@ -2,12 +2,11 @@
 // Подгрузка файлов
 require_once('loader.php');
 
-use Training\ITestMathInterface;
 use Training\ITestSortInterface;
 use Training\Data;
 
 // Класс Test реализует интерфейс ITestSortInterface
-class Test implements ITestSortInterface, ITestMathInterface
+class Test implements ITestSortInterface
 {
     /**
      * Отсортировать одномерный массив своими силами
@@ -82,53 +81,6 @@ class Test implements ITestSortInterface, ITestMathInterface
     {
         // TODO: Implement testSort3() method.
     }
-
-    /**
-     * На входе массив чисел
-     * array(2,5,3,5,6,7,8,9,25,24,18,26,27,28,29,30,31)
-     * Вывести числа кратные 3 и 7 в строку разделенную запятыми
-     *
-     * Ответ '3,6,7...и тд.'
-     */
-    public function testMath1($array)
-    {
-        // TODO: Implement testMath1() method.
-    }
-
-    /**
-     * Нужно сгенерировать случайным образом пароль заданной длинны и из указанных символов.
-     * $symbols - строка с символами
-     * $lenght - длинна пароля
-     *
-     * Пример:
-     * на входе
-     *  $symbols = 'abcdsrRGHK12345'
-     *  $lenght = 5
-     * на выходе строка 'bR48s'
-     */
-    public function testMath2($symbols, $lenght)
-    {
-        // TODO: Implement testMath2() method.
-    }
-
-    /**
-     * Вывести числа Фибоначи, где кол-во цифр задано параметром $n
-     *
-     * На выходе строка к примеру - '0,1,1,2,3,5 и тд.'
-     *
-     * Числа Фибоначи где каждое последующее число
-     *  образуется путем сложения двух предыдущих.
-     * Пример:
-     * 0,1
-     * 0,1,1
-     * 0,1,1,2
-     * 0,1,1,2,3 и тд.
-     */
-    public function testMath3($n)
-    {
-        // TODO: Implement testMath3() method.
-    }
-
 
 }
 
