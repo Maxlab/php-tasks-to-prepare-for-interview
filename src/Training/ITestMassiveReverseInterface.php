a4038fa8dce9b818d57ac3c05349b02a592c59d0
Nikolay Stepanov <forsag1982@gmail.com>
2015-09-30 17:46:02 +0300

    st3

--- src/Training/ITestMassiveReverseInterface.php
+++ src/Training/ITestMassiveReverseInterface.php
@@ -18,7 +18,8 @@ interface ITestMassiveReverseInterface
     /**
      * На входе массив array('A','B','C','D','E')
      * Нужно его перевернуть, но можно использовать, только floor() и count()
-     * из встроенных функций.
+     * из встроенных функций. Перекладывать значения в другой массив нельзя,
+     * используйте, только $array
      */
     public function testMassRev3($array);
 }
